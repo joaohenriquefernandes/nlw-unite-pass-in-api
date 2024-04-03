76d331a1c9037fea14cf4144eb13881c6d954508
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-03 14:54:13 -0300

    feat: create event controller

---
+++ src/controllers/event/CreateController.ts
@@ -0,0 +1,32 @@
+import { FastifyReply, FastifyRequest, RequestGenericInterface } from "fastify";
+import { EventAlreadyExistsError } from "../../services/errors/EventAlreadyExistsError";
+import { makeCreateEventService } from "../../services/factories/MakeCreateEventService";
+
+interface ICreateControllerBodySchemaRequest extends RequestGenericInterface {
+  Body: {
+    details: string
+    maximumAttendees: number
+    title: string
+  }
+}
+
+export async function CreateController(request: FastifyRequest<ICreateControllerBodySchemaRequest>, reply: FastifyReply) {
+  const { details , maximumAttendees, title } = request.body
+
+  const createEventService = makeCreateEventService()
+
+  try {
+
+    const { event } = await createEventService.execute({
+      title,
+      details,
+      maximumAttendees
+    })
+
+    return reply.status(201).send({ event })
+  } catch (error) {
+    if(error instanceof EventAlreadyExistsError) {
+      return reply.status(40).send({ message: error.message })
+    }
+  }
+}
