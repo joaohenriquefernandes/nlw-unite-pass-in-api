9061ae145c1cdabbd73f4c7e3d07dec71810da79
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-03 14:40:32 -0300

    fix: change application port

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { app } from "./app"
-
+import { env } from "./env"
 
 app.listen({
   host: '0.0.0.0',
-  port: 3000
+  port: env.PORT
 }).then(() => {
-  console.log(`HTTP Server Runing at http://localhost:3333`)
+  console.log(`HTTP Server Runing at http://localhost:3000`)
 })
