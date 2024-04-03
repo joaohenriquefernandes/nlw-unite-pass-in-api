d257cc358b8cb8c7cd560ce4909a41de7df6d120
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-03 14:55:00 -0300

    feat: create events route

---
+++ src/controllers/event/routes.ts
@@ -0,0 +1,32 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import z from "zod";
+import { CreateController } from "./CreateController";
+
+export async function eventRoutes(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .post(
+      '/events',
+      {
+        schema: {
+          body: z.object({
+            title: z.string().min(4),
+            details: z.string().nullable(),
+            maximumAttendees: z.number().int().positive().nullable()
+          }),
+          response: {
+            201: z.object({
+              event: z.object({
+                id: z.string().uuid(),
+                title: z.string().min(4),
+                details: z.string().nullable(),
+                maximumAttendees: z.number().int().positive().nullable()
+              })
+            })
+          }
+        },
+      },
+      CreateController
+    )
+}
