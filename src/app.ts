d6e517494d1ea38d69c233ffca8ff46a3c6ab9a0
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-03 14:39:37 -0300

    feat: create error handler

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
-import { routes } from "./routes";
+import { ZodError } from "zod";
+import { eventRoutes } from "./controllers/event/routes";
+import { env } from "./env";
 
 export const app = fastify()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.register(routes)
+app.register(eventRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to a external tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
