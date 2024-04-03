02cbfa2dfbb4c47d9d3c4e290c16317b98be04a7
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-03 14:10:47 -0300

    feat: create event repository

---
+++ src/repositories/EventsRepository.ts
@@ -0,0 +1,28 @@
+import { Event, Prisma } from "@prisma/client";
+import { prisma } from "../libs/prisma";
+import { IEventsRepository } from "./interfaces/IEventsRepository";
+
+export class EventsRepository implements IEventsRepository {
+  async create({ slug, title, details, maximumAttendees }: Prisma.EventUncheckedCreateInput): Promise<Event> {
+    const event = await prisma.event.create({
+      data: {
+        slug,
+        title,
+        details: details || null,
+        maximumAttendees: maximumAttendees || null
+      }
+    })
+
+    return event
+  }
+
+  async findBySlug(slug: string): Promise<Event | null> {
+    const event = await prisma.event.findUnique({
+      where: {
+        slug
+      }
+    })
+
+    return event
+  }
+}
