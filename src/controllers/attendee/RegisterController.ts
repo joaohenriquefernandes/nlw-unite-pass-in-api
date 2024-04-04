00c3f33ee2aa245b6104930498885459c90cf527
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-04 11:08:19 -0300

    feat: update register attendees controller

--- src/controllers/attendee/RegisterController.ts
+++ src/controllers/attendee/RegisterController.ts
@@ -1,4 +1,8 @@
 import { FastifyReply, FastifyRequest, RequestGenericInterface } from "fastify";
+import { EmailAlreadyRegisteredError } from "../../services/errors/EmailAlreadyRegisteredError";
+import { EventNotFoundError } from "../../services/errors/EventNotFoundError";
+import { MaximumNumberReachedError } from "../../services/errors/MaximumNumberReachedError";
+import { makeRegisterForEventService } from "../../services/factories/MakeRegisterForEventService";
 
 interface IRegisterControllerSchemaRequest extends RequestGenericInterface {
   Body: {
@@ -10,8 +14,30 @@ interface IRegisterControllerSchemaRequest extends RequestGenericInterface {
   }
 }
 
-export function RegisterController(request: FastifyRequest<IRegisterControllerSchemaRequest>, reply: FastifyReply) {
+export async function RegisterController(request: FastifyRequest<IRegisterControllerSchemaRequest>, reply: FastifyReply) {
   const { eventId } = request.params
 
   const { email, name } = request.body
+
+  const registerForEventService = makeRegisterForEventService()
+
+  try {
+    const { attendee } = await registerForEventService.execute({
+      email,
+      eventId,
+      name
+    })
+
+    return reply.status(201).send({ attendee })
+  } catch (error) {
+    if(error instanceof EmailAlreadyRegisteredError || error instanceof MaximumNumberReachedError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    if(error instanceof EventNotFoundError) {
+      return reply.status(404).send({ message: error.message })
+    }
+  }
+
+
 }
