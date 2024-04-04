ee8b4adb54273759914cdaa9ce42c97b4a4c63d2
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-04 10:55:25 -0300

    feat: create find event by id method

--- src/repositories/interfaces/IEventsRepository.ts
+++ src/repositories/interfaces/IEventsRepository.ts
@@ -2,5 +2,6 @@ import { Event, Prisma } from "@prisma/client";
 
 export interface IEventsRepository {
   create: ({ slug, title, details, maximumAttendees }: Prisma.EventUncheckedCreateInput) => Promise<Event>;
+  findById: (id: string) => Promise<Event | null>
   findBySlug: (slug: string) => Promise<Event | null>
 }
