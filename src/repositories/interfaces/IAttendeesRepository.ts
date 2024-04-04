3d9197f966bec21ee3e0cc4f87df8040444598dd
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-04 10:56:41 -0300

    feat: create find by email and count by id attendees

--- src/repositories/interfaces/IAttendeesRepository.ts
+++ src/repositories/interfaces/IAttendeesRepository.ts
@@ -2,4 +2,6 @@ import { Attendee, Prisma } from "@prisma/client";
 
 export interface IAttendeesRepository {
   create: ({ email, eventId, name }: Prisma.AttendeeUncheckedCreateInput) => Promise<Attendee>
+  findByEmail: (email: string, eventId: string) => Promise<Attendee | null>
+  countByEvent: (eventId: string) => Promise<number>
 }
