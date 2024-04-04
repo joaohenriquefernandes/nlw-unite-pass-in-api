7e99122bf2e67be6cd12b4f0bf1d4ec827c64ef2
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-04 11:06:43 -0300

    feat: update register for event service

--- src/services/RegisterForEventService.ts
+++ src/services/RegisterForEventService.ts
@@ -1,5 +1,9 @@
 import { Attendee } from "@prisma/client";
 import { IAttendeesRepository } from "../repositories/interfaces/IAttendeesRepository";
+import { IEventsRepository } from "../repositories/interfaces/IEventsRepository";
+import { EmailAlreadyRegisteredError } from "./errors/EmailAlreadyRegisteredError";
+import { EventNotFoundError } from "./errors/EventNotFoundError";
+import { MaximumNumberReachedError } from "./errors/MaximumNumberReachedError";
 
 interface IRegisterForEventServiceRequest {
   name: string;
@@ -12,9 +16,31 @@ interface IRegisterForEventServiceResponse {
 }
 
 export class RegisterForEventService {
-  constructor(private attendeesRepository: IAttendeesRepository) {}
+  constructor(
+    private attendeesRepository: IAttendeesRepository,
+    private eventsRepository: IEventsRepository
+  ) {}
 
   async execute({email, eventId, name}: IRegisterForEventServiceRequest): Promise<IRegisterForEventServiceResponse> {
+    const attendeeFromEmail = await this.attendeesRepository.findByEmail(email, eventId)
+
+    if(attendeeFromEmail) {
+      throw new EmailAlreadyRegisteredError()
+    }
+
+    const [event, amountOfAttendeesForEvent] = await Promise.all([
+      this.eventsRepository.findById(eventId),
+      this.attendeesRepository.countByEvent(eventId)
+    ])
+
+    if(!event) {
+      throw new EventNotFoundError()
+    }
+
+    if(event?.maximumAttendees && amountOfAttendeesForEvent >= event.maximumAttendees) {
+      throw new MaximumNumberReachedError()
+    }
+
     const attendee = await this.attendeesRepository.create({
       email,
       eventId,
