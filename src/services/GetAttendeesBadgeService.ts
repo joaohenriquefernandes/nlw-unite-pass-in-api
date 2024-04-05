f1d62e81733389c068a36a03aae53b68d0205f7f
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-05 15:36:43 -0300

    feat: update get attendee badge service with url

--- src/services/GetAttendeesBadgeService.ts
+++ src/services/GetAttendeesBadgeService.ts
@@ -4,22 +4,33 @@ import { AttendeeNotFoundError } from "./errors/AttendeeNotFoundError";
 
 interface IGetAttendeesBadgeServiceRequest {
   id: number
+  baseURL: string
 }
 
 interface IGetAttendeesBadgeServiceResponse {
-  attendee: IFindByIdParamsResponse
+  badge:{
+    attendee: IFindByIdParamsResponse
+    checkInURL: string
+  }
 }
 
 export class GetAttendeesBadgeService {
   constructor(private attendeesRepository: AttendeesRepository){}
 
-  async execute({ id }: IGetAttendeesBadgeServiceRequest): Promise<IGetAttendeesBadgeServiceResponse> {
+  async execute({ id, baseURL }: IGetAttendeesBadgeServiceRequest): Promise<IGetAttendeesBadgeServiceResponse> {
     const attendee = await this.attendeesRepository.findById(id)
 
     if(!attendee) {
       throw new AttendeeNotFoundError()
     }
 
-    return { attendee }
+    const checkInURL = new URL(`/attendees/${attendee.id}/check-in`, baseURL)
+
+    const badge = {
+      attendee,
+      checkInURL: checkInURL.toString()
+    }
+
+    return { badge }
   }
 }
