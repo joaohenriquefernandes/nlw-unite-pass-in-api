c0dd833f20d34324a061916ddef764c520c92585
joaohenriquefernandes <jh.fernandes12@outlook.com>
2024-04-05 16:51:54 -0300

    feat: add find many by event id method in interface

--- src/repositories/interfaces/IAttendeesRepository.ts
+++ src/repositories/interfaces/IAttendeesRepository.ts
@@ -7,13 +7,24 @@ export interface IFindByIdParamsResponse {
   id: number;
   createdAt: Date;
   event: {
-      title: string;
+    title: string;
   };
+}
+
+export interface IFindManyByEventIdParamsResponse {
+  id: number,
+  name: string,
+  email: string,
+  createdAt: Date,
+  checkIn: {
+    createdAt: Date
+  } | null
 }
 
 export interface IAttendeesRepository {
   create: ({ email, eventId, name }: Prisma.AttendeeUncheckedCreateInput) => Promise<Attendee>
   findByEmail: (email: string, eventId: string) => Promise<Attendee | null>
   findById: (id: number) => Promise<IFindByIdParamsResponse | null>
+  findManyByEventId: (eventId: string, pageIndex: number, query?: string) => Promise<IFindManyByEventIdParamsResponse[]>
   countByEvent: (eventId: string) => Promise<number>
 }
